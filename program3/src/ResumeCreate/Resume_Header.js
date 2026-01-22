11037fb85509c019dc91b6a68b81650bbec1707e
krsameer <skateboard.maniac18@gmail.com>
2026-01-22 21:01:57 +0530

    Added Program3 - Resume of a job aspirant using React components like classes and functions

---
+++ program3/src/ResumeCreate/Resume_Header.js
@@ -0,0 +1,13 @@
+// Designed a resume of a job aspirant using React components like classes and functions
+
+import React from "react";
+
+class Resume_Header extends React.Component {
+  render() {
+    return (
+      <h1 className="header">Resume Details</h1>
+    );
+  }
+}
+
+export default Resume_Header;
