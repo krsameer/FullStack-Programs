737432ce77eec21504346b6c0c465d04b157c0ec
krsameer <skateboard.maniac18@gmail.com>
2026-01-23 11:22:23 +0530

    Added resume profile in program3

---
+++ program3/src/Resume_PersonInfo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Resume_Person_Info extends React.Component {
+  render() {
+    return (
+      <div className="person-info">
+        <p><b>Name:</b> Kumar Sameer</p>
+        <p><b>Address:</b> Bengaluru</p>
+        <p><b>Phone No:</b> 7739884369</p>
+        <p><b>Email:</b> kumarsameer.mca24@rvce.edu.in</p>
+      </div>
+    );
+  }
+}
+
+export default Resume_Person_Info;
