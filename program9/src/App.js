55ee3cbcbeab11725e30c1ff5cdef563552ba5cd
krsameer <skateboard.maniac18@gmail.com>
2026-02-08 17:48:23 +0530

    Added employee form

--- program9/src/App.js
+++ program9/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const API_URL="http://localhost:5000/employees";
 
-export default App;
+function App(){
+  const [employees,setEmployees] = useState([]);
+  const [currentEmployee, setCurrentEmployee] = useState(null);
+
+  useEffect(() => {
+    fetchEmployees();
+}, []);
+
+const fetchEmployees=async() => {
+  const response=await axios.get(API_URL);
+  setEmployees(response.data);
+};
+
+const handleSave=async(employee) => {
+  if(employee.id) {
+    await axios.put(`${API_URL}/${employee.id}`,employee);
+  } else {
+    await axios.post(API_URL, employee);
+  }
+  fetchEmployees();
+  setCurrentEmployee(null);
+};
+
+
