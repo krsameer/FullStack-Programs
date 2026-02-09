260b95321a2aa2082da976110153b46d23b9b56f
krsameer <skateboard.maniac18@gmail.com>
2026-02-09 08:25:03 +0530

    Created a React application using react-router-dom with multiple pages (Home, About, Contact)

---
+++ program10/src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function About() {
+return (
+<div>
+<h1>About Us</h1>
+<p>This is the about page, where you can learn more about us.</p>
+</div>
+);
+}
+export default About;
